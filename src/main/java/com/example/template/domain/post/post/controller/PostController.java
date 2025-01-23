4395bbb637a0abe183898632fccbf6c5bfdfeee5
chacha86 <taejincha0619@gmail.com>
2025-01-23 15:50:05 +0900

    modelAttribute apply

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -1,7 +1,9 @@
 package com.example.template.domain.post.post.controller;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -9,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/posts")
-@Validated
 public class PostController {
 
     @GetMapping("/write")
@@ -18,18 +19,24 @@ public class PostController {
         return getFormHtml("");
     }
 
+    @AllArgsConstructor
+    @Getter
+    public static class WriteForm {
+        @NotBlank(message = "제목을 입력해주세요.") @Length(min=5, message = "제목은 5글자 이상입니다.")
+        private String title;
+        @NotBlank(message = "내용을 입력해주세요.") @Length(min=10, message = "내용은 10글자 이상입니다.")
+        private String content;
+    }
+
     @PostMapping("/write")
     @ResponseBody
-    public String doWrite(
-            @NotBlank @Length(min=5) String title,
-            @NotBlank @Length(min=10) String content
-    ) {
+    public String doWrite(@ModelAttribute @Valid WriteForm form) {
 
         return """
                 <h1>게시물 조회</h1>
                 <div>%s</div>
                 <div>%s</div>
-                """.formatted(title, content);
+                """.formatted(form.title, form.content);
     }
 
     private String getFormHtml(String errorMsg) {

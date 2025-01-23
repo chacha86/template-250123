45eeebf6a611ee31ceb694f62c652bff864cc24e
chacha86 <taejincha0619@gmail.com>
2025-01-23 14:26:24 +0900

    doWrite

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -3,6 +3,7 @@ package com.example.template.domain.post.post.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -13,12 +14,23 @@ public class PostController {
     @ResponseBody
     public String showWrite() {
         return """
-                <form action="">
+                <form action="/posts/doWrite">
                   <input type="text" name="title" placeholder="제목" />
                   <textarea name="content"></textarea>
                   <input type="submit" value="등록" />
                 </form>
                 """;
+    }
+
+    @GetMapping("/doWrite")
+    @ResponseBody
+    public String doWrite(String title, String content) {
+
+        return """
+                <h1>게시물 조회</h1>
+                <div>%s</div>
+                <div>%s</div>
+                """.formatted(title, content);
     }
 
 }

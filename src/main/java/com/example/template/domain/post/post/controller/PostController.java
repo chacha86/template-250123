f02e11bd66d2a8dd76146a818feeb4ec6fc4ce07
chacha86 <taejincha0619@gmail.com>
2025-01-23 14:14:20 +0900

    show write

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -2,15 +2,23 @@ package com.example.template.domain.post.post.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/posts")
 public class PostController {
 
-    @GetMapping("/")
+    @GetMapping("/write")
     @ResponseBody
-    public String hello() {
-        return "hello";
+    public String showWrite() {
+        return """
+                <form action="">
+                  <input type="text" name="title" placeholder="제목" />
+                  <textarea name="content"></textarea>
+                  <input type="submit" value="등록" />
+                </form>
+                """;
     }
 
 }

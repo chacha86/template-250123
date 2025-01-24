aec3ef9b371afe0872bf92f256119d641d9a37a0
chacha86 <taejincha0619@gmail.com>
2025-01-24 11:49:24 +0900

    list.html

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -91,20 +91,8 @@ public class PostController {
         return "redirect:/posts"; //리다이렉트
     }
 
-    private String getFormHtml(String errorMsg, String title, String content) {
-        return """
-                <div>%s</div>
-                <form method="post">
-                  <input type="text" name="title" placeholder="제목" value="%s"/> <br>
-                  <textarea name="content">%s</textarea> <br>
-                  <input type="submit" value="등록" /> <br>
-                </form>
-                """.formatted(errorMsg, title, content);
-    }
-
     @GetMapping
-    @ResponseBody
-    private String showList() {
+    private String showList(Model model) {
 
         String lis = posts.stream()
                 .map(p -> "<li>" + p.getTitle() + "</li>")
@@ -112,14 +100,9 @@ public class PostController {
 
         String ul = "<ul>" + lis + "</ul>";
 
+        model.addAttribute("posts", posts);
 
-        return """
-                <div>글 목록</div>
-                
-                %s
-                
-                <a href="/posts/write">글쓰기</a>
-                """.formatted(ul);
+        return "domain/post/post/list";
     }
 
 
